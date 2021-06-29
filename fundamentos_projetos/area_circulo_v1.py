141a2dbeeb1d4e207f5cbbb438cce7fa8de39bc1
hugomacielads <hugomacielcesar@gmail.com>
2021-06-28 22:50:57 -0300

    (28/06) Projeto introdutório

---
+++ fundamentos_projetos/area_circulo_v1.py
@@ -0,0 +1,5 @@
+# Projeto de python introdutório
+
+pi = 3.14159
+raio = 15
+print('Área do circulo', pi * raio ** 2)
---
+++ fundamentos_projetos/area_circulo_v3.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+pi = 3.14159
+raio = 15
+print('Área do circulo', pi * raio ** 2)
