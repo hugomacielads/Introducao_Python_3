4c5244dc3576ad72a95fb47b7c8a6215f32c3301
hugomacielads <hugomacielcesar@gmail.com>
2021-06-28 22:52:09 -0300

    (28/06) Interpolação

--- fundamentos/11-interpolacao.py
+++ fundamentos/11-interpolacao.py
@@ -4,6 +4,8 @@ nome, idade = 'Ana', 30.9875
 
 print('Nome: %s Idade: %.2f' % (nome, idade))
 print('Nome: {0} Idade: {1}'.format(nome, idade))
+
+# Método mais utilizado
 print(f'Nome: {nome} Idade: {idade}')
 
 s = Template('Nome: $nome Idade: $idade')
