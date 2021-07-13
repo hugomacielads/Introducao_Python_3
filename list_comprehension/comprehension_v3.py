da95d97f247bd9628d54c6ab721014a61a93b2ca
hugomacielads <hugomacielcesar@gmail.com>
2021-07-13 01:32:31 -0300

    (13/07) Funções - Gerador HTML

---
+++ list_comprehension/comprehension_v3.py
@@ -0,0 +1,8 @@
+generator = (i ** 2 for i in range(10) if i % 2 == 0)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+
+# Gera o código sobre demanda e utiliza muito menos memória
